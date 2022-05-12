6a975773c672320614a7ca2449b8fbcfdf5adf9e
qzart45hi <qwert45hi@yandex.ru>
2022-05-12 13:47:51 +0300

    fix: add the latest commits to __init__

---
+++ comath/equations/__init__.py
@@ -0,0 +1,3 @@
+from .interfaces import AnyEquation, LambdaEquation, SimpleFunction
+from .functions import LinearEquation, SquareEquation, PolynomialEquation, TrigonometricEquation
+from .functions import TrigonometricEquationType, ExponentEquation, LogarithmEquation
